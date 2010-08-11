d6f584e1035613ad278e2032976d04c9285d0d38
Quinn Slack <sqs@cs.stanford.edu>
2010-08-11 13:50:36 +0800

    bsd ipfw setup

--- user/launch_tcpcryptd.sh
+++ user/launch_tcpcryptd.sh
@@ -16,7 +16,6 @@ ee() {
 }
 
 linux_set_iptables() {
-    echo Tcpcrypting port 80 and all local traffic...
     ee iptables -I INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
     ee iptables -I OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
     ee iptables -I INPUT  -p tcp -i lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
@@ -33,11 +32,14 @@ linux_unset_iptables() {
 }
 
 bsd_set_ipfw() {
-    echo Tcpcrypting port 80 and all local traffic...
+    ipfw -q 01 add divert $TCPCRYPTD_DIVERT_PORT tcp from any 80 to any
+    ipfw -q 02 add divert $TCPCRYPTD_DIVERT_PORT tcp from any to any 80
+    ipfw -q 03 add divert $TCPCRYPTD_DIVERT_PORT tcp from any to any via lo0
 }
 
 bsd_unset_ipfw() {
     echo Removing ipfw rules and quitting tcpcryptd...
+    ipfw delete 01 02 03
 }
 
 if [ `whoami` != "root" ]
@@ -46,16 +48,19 @@ then
     exit 1
 fi
 
+echo Tcpcrypting port 80 and all local traffic...
+
 case "$OSNAME" in
     Linux)
         linux_set_iptables
         trap linux_unset_iptables 2 # trap SIGINT to remove iptables rules before exit
         start_tcpcryptd
         linux_unset_iptables
+        ;;
     FreeBSD|Darwin)
         bsd_set_ipfw
         trap bsd_unset_ipfw 2
         start_tcpcryptd
-        bsd_unset_iptables
+        bsd_unset_ipfw
 esac
 
