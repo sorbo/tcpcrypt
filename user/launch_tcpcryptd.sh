1359b6900d33b2bed2fa2ba6db1e6dad25257dad
Quinn Slack <sqs@cs.stanford.edu>
2010-08-10 21:22:16 -0700

    bsd stubs and formatting

--- user/launch_tcpcryptd.sh
+++ user/launch_tcpcryptd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 OSNAME=`uname -s`
-ENCRYPT_PORT=80
+PORT=${1:-80}
 
 TCPCRYPTD=`dirname $0`/tcpcrypt/tcpcryptd
 TCPCRYPTD_DIVERT_PORT=666
@@ -17,19 +17,27 @@ ee() {
 
 linux_set_iptables() {
     echo Tcpcrypting port 80 and all local traffic...
-    ee iptables -I INPUT -p tcp --sport $ENCRYPT_PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I OUTPUT -p tcp --dport $ENCRYPT_PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I INPUT -p tcp -i lo -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I OUTPUT -p tcp -o lo -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    ee iptables -I INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    ee iptables -I OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    ee iptables -I INPUT  -p tcp -i lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    ee iptables -I OUTPUT -p tcp -o lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
 }
 
 linux_unset_iptables() {
     echo Removing iptables rules and quitting tcpcryptd...
-    iptables -D INPUT -p tcp --sport $ENCRYPT_PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D OUTPUT -p tcp --dport $ENCRYPT_PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D INPUT -p tcp -i lo -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D OUTPUT -p tcp -o lo $ENCRYPT_PORT2 -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    iptables -D INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    iptables -D OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    iptables -D INPUT  -p tcp -i lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    iptables -D OUTPUT -p tcp -o lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
     exit
+}
+
+bsd_set_ipfw() {
+    echo Tcpcrypting port 80 and all local traffic...
+}
+
+bsd_unset_ipfw() {
+    echo Removing ipfw rules and quitting tcpcryptd...
 }
 
 if [ `whoami` != "root" ]
@@ -44,5 +52,10 @@ case "$OSNAME" in
         trap linux_unset_iptables 2 # trap SIGINT to remove iptables rules before exit
         start_tcpcryptd
         linux_unset_iptables
+    FreeBSD|Darwin)
+        bsd_set_ipfw
+        trap bsd_unset_ipfw 2
+        start_tcpcryptd
+        bsd_unset_iptables
 esac
 
