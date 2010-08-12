0c8fa289788d533fd6ad895ab8f81a1f609a2099
Quinn Slack <sqs@cs.stanford.edu>
2010-08-12 15:51:29 -0700

    merge branch master

--- README.markdown
+++ README.markdown
@@ -1,7 +1,20 @@
 Tcpcrypt
 ========
 
+<<<<<<< HEAD
+Tcpcrypt is a protocol that attempts to encrypt (almost) all of your network
+traffic. Unlike other security mechanisms, Tcpcrypt works out of the box: it
+requires no configuration, no changes to applications, and your network
+connections will continue to work even if the remote end does not support
+Tcpcrypt, in which case connections will gracefully fall back to standard
+clear-text TCP.
+
+Tcpcrypt supports Linux, Mac OS X, Windows, and FreeBSD.
+
+For more information, see the [tcpcrypt.org](http://tcpcrypt.org).
+=======
 [Tcpcrypt homepage](http://tcpcrypt.org)
+>>>>>>> 0886a44356ca4b87e8bee6a23daf79d071e74a9b
 
 Installing tcpcrypt
 -------------------
@@ -9,10 +22,37 @@ Installing tcpcrypt
     git clone git://github.com/sorbo/tcpcrypt.git
     cd tcpcrypt/user
     make
+<<<<<<< HEAD
+    ./launch_tcpcryptd.sh
+
+The launch script starts tcpcryptd and adds firewall rules to divert Web and local port 7777 traffic to tcpcryptd.
+
+On Linux, you must first install libnfnetlink, libnetfilter_queue, and libcap.
+
+Try it out
+---------- 
+
+Go to [http://tcpcrypt.org/test.php](http://tcpcrypt.org/test.php) with
+tcpcryptd running. If tcpcrypt is working, you'll be able to join the
+tcpcrypt Hall of Fame and your tcpcrypt session ID will be displayed at the
+bottom of the page.
+
+Now let's examine the packets going over the wire by starting tcpdump and then
+reloading the URL above.
+
+    sudo tcpdump -X -s0 host tcpcrypt.org
+
+Compare this tcpdump output, which appears encrypted (or at least unreadable),
+with the cleartext packets you would see without tcpcryptd running.
+
+A final netcat example:
+
+=======
 
 Test drive
 ---------- 
 
+>>>>>>> 0886a44356ca4b87e8bee6a23daf79d071e74a9b
     # in tcpcrypt/user
     sudo ./launch_tcpcryptd.sh & 
     nc -l 7777 &
@@ -22,12 +62,21 @@ Test drive
     # clean up
     sudo killall tcpcryptd tcpdump
 
+<<<<<<< HEAD
+=======
 You can also go to [http://tcpcrypt.org/fame.php](http://tcpcrypt.org/fame.php)
 while tcpcryptd is running. You'll see a text box where you can post a message
 to the tcpcrypt Hall of Fame, and your tcpcrypt session ID is displayed at the
 bottom of the page.
+>>>>>>> 0886a44356ca4b87e8bee6a23daf79d071e74a9b
 
 More info
 ---------
 
+<<<<<<< HEAD
+The INSTALL-* files have more detailed installation and firewall setup instructions. See [tcpcrypt.org](http://tcpcrypt.org) for general info, including the [protocol specification](http://tcpcrypt.org/docs.php).
+
+The code repository lives at [http://github.com/sorbo/tcpcrypt](http://github.com/sorbo/tcpcrypt).
+=======
 ... TODO ...
+>>>>>>> 0886a44356ca4b87e8bee6a23daf79d071e74a9b
--- user/launch_tcpcryptd.sh
+++ user/launch_tcpcryptd.sh
@@ -2,12 +2,13 @@
 
 OSNAME=`uname -s`
 PORT=${1:-80}
+PORT2=${2:-7777}
 
 TCPCRYPTD=`dirname $0`/tcpcrypt/tcpcryptd
-TCPCRYPTD_DIVERT_PORT=666
+DIVERT_PORT=666
 
 start_tcpcryptd() {
-    LD_LIBRARY_PATH=lib/ $TCPCRYPTD $OPTS -p $TCPCRYPTD_DIVERT_PORT
+    LD_LIBRARY_PATH=lib/ $TCPCRYPTD $OPTS -p $DIVERT_PORT
 }
 
 ee() {
@@ -16,51 +17,99 @@ ee() {
 }
 
 linux_set_iptables() {
-    ee iptables -I INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I INPUT  -p tcp -i lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    ee iptables -I OUTPUT -p tcp -o lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    echo Tcpcrypting port 80 and local traffic on port 7777...
+    ee iptables -I INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $DIVERT_PORT
+    ee iptables -I OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $DIVERT_PORT
+    ee iptables -I INPUT  -p tcp --dport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    ee iptables -I INPUT  -p tcp --sport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    ee iptables -I OUTPUT -p tcp --dport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    ee iptables -I OUTPUT -p tcp --sport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
 }
 
 linux_unset_iptables() {
     echo Removing iptables rules and quitting tcpcryptd...
-    iptables -D INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D INPUT  -p tcp -i lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
-    iptables -D OUTPUT -p tcp -o lo         -j NFQUEUE --queue-num $TCPCRYPTD_DIVERT_PORT
+    iptables -D INPUT  -p tcp --sport $PORT -j NFQUEUE --queue-num $DIVERT_PORT
+    iptables -D OUTPUT -p tcp --dport $PORT -j NFQUEUE --queue-num $DIVERT_PORT
+    iptables -D INPUT  -p tcp --dport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    iptables -D INPUT  -p tcp --sport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    iptables -D OUTPUT -p tcp --dport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
+    iptables -D OUTPUT -p tcp --sport $PORT2 -j NFQUEUE --queue-num $DIVERT_PORT
     exit
 }
 
 bsd_set_ipfw() {
-    ipfw -q 01 add divert $TCPCRYPTD_DIVERT_PORT tcp from any 80 to any
-    ipfw -q 02 add divert $TCPCRYPTD_DIVERT_PORT tcp from any to any 80
-    ipfw -q 03 add divert $TCPCRYPTD_DIVERT_PORT tcp from any to any via lo0
+    echo Tcpcrypting port 80 and local traffic on port 7777...
+    ipfw 02 add divert $DIVERT_PORT tcp from any to any $PORT
+    ipfw 03 add divert $DIVERT_PORT tcp from any $PORT to any
+    ipfw 04 add divert $DIVERT_PORT tcp from any to any $PORT2 via lo0
+    ipfw 05 add divert $DIVERT_PORT tcp from any $PORT2 to any via lo0
 }
 
 bsd_unset_ipfw() {
     echo Removing ipfw rules and quitting tcpcryptd...
-    ipfw delete 01 02 03
+    ipfw delete 02 03 04 05
+    exit
 }
 
-if [ `whoami` != "root" ]
-then
-    echo "must be root"
-    exit 1
-fi
+win_start_tcpcryptd() {
+    MAC_ADDR=`ipconfig /all | grep 'Physical Address'| head -n 1 | sed 's/\s*Physical Address\(\. \)*: \(.*\)/\2/' | sed 's/-/:/g'`
+    echo Using MAC address $MAC_ADDR...
+    LD_LIBRARY_PATH=lib/ $TCPCRYPTD $OPTS -p $DIVERT_PORT -x $MAC_ADDR
+}
 
-echo Tcpcrypting port 80 and all local traffic...
+check_root() {
+    if [ `whoami` != "root" ]
+    then
+        echo "must be root"
+        exit 1
+    fi
+}
+
+check_ssh() {
+    if [ -n "$SSH_CONNECTION" ]
+    then
+        read -p 'Command may disrupt existing ssh connections. Proceed? [y/N] ' C
+        if [ "$C" != "y" ]
+        then
+            exit 1
+        fi
+    fi
+}
+
+check_existing_tcpcryptd() {
+    P=`ps axo pid,comm | grep tcpcryptd`
+    if [ $? -eq 0 ]
+    then
+        read -p "tcpcryptd already running with pid $P. Proceed? [y/N] " C
+        if [ "$C" != "y" ]
+        then
+            exit 1
+        fi
+    fi
+}
+
+
+#check_ssh
 
 case "$OSNAME" in
     Linux)
+        check_existing_tcpcryptd
+        check_root
         linux_set_iptables
         trap linux_unset_iptables 2 # trap SIGINT to remove iptables rules before exit
         start_tcpcryptd
         linux_unset_iptables
         ;;
     FreeBSD|Darwin)
+        check_existing_tcpcryptd
+        check_root
         bsd_set_ipfw
         trap bsd_unset_ipfw 2
         start_tcpcryptd
         bsd_unset_ipfw
+        ;;
+    [Cc][Yy][Gg][Ww][Ii][Nn]*)
+        win_start_tcpcryptd
+        ;;
 esac
 
