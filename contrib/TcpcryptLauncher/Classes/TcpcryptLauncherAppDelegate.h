8d61fed01869d406df8bd9e1e0f2b2c9bbf7c51e
Quinn Slack <qslack@qslack.com>
2010-08-17 14:12:51 -0700

    xcode project template for tcpcryptlauncher

---
+++ contrib/TcpcryptLauncher/Classes/TcpcryptLauncherAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TcpcryptLauncherAppDelegate.h
+//  TcpcryptLauncher
+//
+//  Created by Samuel Quinn Slack on 8/17/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+@interface TcpcryptLauncherAppDelegate : NSObject <NSApplicationDelegate> {
+
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
+
