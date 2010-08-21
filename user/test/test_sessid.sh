e31fa792b5d57fb183384c2ce7efef891398a1ce
Quinn Slack <sqs@cs.stanford.edu>
2010-08-21 00:45:06 -0700

    kill tcpcryptd

--- user/test/test_sessid.sh
+++ user/test/test_sessid.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+PIDFILE=/tmp/tcpcrypt.pid
 
 `dirname $0`/../launch_tcpcryptd.sh &
 sleep 2
 RES=$(curl http://tcpcrypt.org/sessid.php 2>/dev/null | grep -v NONE)
 RET=$?
-killall tcpcryptd > /dev/null
+kill `cat $PIDFILE 2>/dev/null` > /dev/null 2>&1
+rm -f $PIDFILE
 echo "$RES"
-return $RET
+exit $RET
