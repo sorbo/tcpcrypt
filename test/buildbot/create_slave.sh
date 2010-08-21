743bdc36086b90d3b66329516fa813fd0d14cfc0
Quinn Slack <sqs@cs.stanford.edu>
2010-08-20 23:11:47 -0700

    option to use new buildslave name for executable

--- test/buildbot/create_slave.sh
+++ test/buildbot/create_slave.sh
@@ -7,13 +7,14 @@ SECRET=secret
 MASTER=hs02.scs.stanford.edu:9989
 BUILDDIR=/tmp/tcbuild
 
+BUILDSLAVE=${BUILDSLAVE-buildbot} # new buildbot vers use buildslave
 SSH="ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i $PRIVATE_KEY root@$HOST"
 
 echo creating buildbot $NAME on $HOST
 $SSH rm -rf $BUILDDIR/buildbot.tac
 $SSH mkdir -p $BUILDDIR/build/$NAME
-$SSH buildbot create-slave $BUILDDIR $MASTER $NAME $SECRET && \
+$SSH $BUILDSLAVE create-slave $BUILDDIR $MASTER $NAME $SECRET && \
 $SSH eval "uname -a > $BUILDDIR/info/host" && \
 (echo `git config --get user.name` "<"`git config --get user.email`">" | $SSH eval "cat > $BUILDDIR/info/admin") && \
-$SSH eval "cd $BUILDDIR && buildbot restart $BUILDDIR" && \
+$SSH eval "cd $BUILDDIR && $BUILDSLAVE restart $BUILDDIR" && \
 echo buildbot $NAME started on $HOST
